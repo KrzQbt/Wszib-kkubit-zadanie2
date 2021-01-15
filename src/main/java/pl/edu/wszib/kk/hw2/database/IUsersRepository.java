5129c260496462b28954a307c4923e4cdedc4f1a
KrzQbt <62937499+KrzQbt@users.noreply.github.com>
2021-01-15 01:29:07 +0100

    Delete src/main/java/pl/edu/wszib/kk/hw2/database directory

--- src/main/java/pl/edu/wszib/kk/hw2/database/IUsersRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.wszib.kk.hw2.database;
-
-
-import pl.edu.wszib.kk.hw2.model.User;
-
-public interface IUsersRepository {
-    User authenticate(User user);
-    boolean register(User user);
-}
